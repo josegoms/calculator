edf34333c04b5d4546f9f61ea6614b23462f30e9
josegoms <josegomespinheiro26@gmail.com>
2025-03-15 23:28:08 -0300

    Add the calculator logic with 2 or more numbers

--- scripts.js
+++ scripts.js
@@ -1,19 +1,92 @@
-function add() {
-    return
+function add(n1, n2) {
+    let operation = Number(n1) + Number(n2);
+    return operation;
 }
-function subtract() {
-    return
+function subtract(n1, n2) {
+    let operation = Number(n1) - Number(n2);
+    return operation;
 }
-function multiply() {
-    return
+function multiply(n1, n2) {
+    let operation = Number(n1) * Number(n2);
+    return operation;
 }
-function divide() {
-    return
+function divide(n1, n2) {
+    let operation = Number(n1) / Number(n2);
+    return operation;
 }
 function operate(n1, op, n2) {
-    return
+    let result;
+
+    switch (op) {
+        case "+":
+            result = add(n1, n2);
+            break;
+        case "-":
+            result = subtract(n1, n2);
+            break;
+        case "/":
+            result = divide(n1, n2);
+            break;
+        case "*":
+            result = multiply(n1, n2);
+            break;
+    }
+    return result;
 }
 
 let num1;
-let operator;
 let num2;
+let operator;
+let result;
+
+const numbers = document.querySelector(".numbers");
+const signal = document.querySelector(".add-buttons");
+const panel = document.querySelector(".display");
+
+numbers.addEventListener("click", (event) => {
+    if (operator === undefined) {
+
+        if (num1 === undefined) {
+            num1 = event.target.value;
+            panel.textContent = `${num1}`;
+        } else {
+            num1 += event.target.value;
+            panel.textContent = `${num1}`;
+        }
+    } 
+    else if (operator !== undefined) {
+        if (num2 === undefined) {
+            num2 = event.target.value;
+            panel.textContent += `${num2}`;
+        } else {
+            num2 += event.target.value;
+            panel.textContent = "";
+            panel.textContent = `${num1}${operator}${num2}`;
+        }
+    }
+});
+
+signal.addEventListener("click", (event) => {
+    if (event.target.value === "clear") {
+        num1 = undefined;
+        num2 = undefined;
+        operator = undefined;
+        panel.textContent = "";
+    }
+    else if (event.target.value === "=") {
+        result = operate(num1, operator, num2);
+        panel.textContent = `${result}`;
+    }
+    else if (event.target.value !== "=" && operator === undefined) {
+        operator = event.target.value;
+        panel.textContent += `${operator}`;
+    }
+    else if (operator !== undefined && num2 !== undefined) {
+        result = operate(num1, operator, num2);
+        num1 = result;
+        num2 = undefined;
+        operator = event.target.value;
+        panel.textContent = `${num1}${operator}`;
+    }
+});
+
