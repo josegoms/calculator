eb5a9a0e6d694936eb7bb861b4e680f0ba6ecf42
josegoms <josegomespinheiro26@gmail.com>
2025-03-16 21:21:20 -0300

    Fix bug with function keys acting like numbers

--- scripts.js
+++ scripts.js
@@ -140,6 +140,11 @@ signal.addEventListener("click", handleAddButtonsCase);
 window.addEventListener("keydown",(event) => {
     const allowedNumbersKeys = [".", "Backspace"];
     const allowedAddButtonKeys = ["Delete", "+", "-", "*", "/", "Enter"];
+    const functionKeys = ["F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12"];
+    
+    if (functionKeys.includes(event.key)) {
+        return;
+    }
     if (/\d/.test(event.key) || allowedNumbersKeys.includes(event.key)) {
         handleNumbersSection(event);
     }
